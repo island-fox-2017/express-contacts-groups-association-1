d1a6ca70baf1f7650ce415c0f51dee1d18385fb5
pdvega <pdvegaa@gmail.com>
2017-07-10 21:33:18 +0700

    profile done

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.get('/contacts/delete/:id', function(req, res){
 })
 
 app.get('/contacts/edit/:id', function(req, res) {
-  db.all(`SELECT * FROM Contacts WHERE id=${req.params.id};`, function(err, rows){
-    res.render('editContacts',{dataContact : rows})
+  db.get(`SELECT * FROM Contacts WHERE id=${req.params.id};`, function(err, data){
+    res.render('editContacts',{dataContact: data})
   });
 })
 
@@ -64,7 +64,7 @@ app.get('/groups/delete/:id', function(req, res){
 })
 
 app.get('/groups/edit/:id', function(req, res){
-  db.all(`SELECT * FROM Groups WHERE id=${req.params.id};`, function(err, datas){
+  db.get(`SELECT * FROM Groups WHERE id=${req.params.id};`, function(err, datas){
     res.render('editGroups', {dataGroup : datas})
   })
 })
@@ -72,6 +72,47 @@ app.get('/groups/edit/:id', function(req, res){
 app.post('/groups/edit/:id', function(req, res){
   db.run(`UPDATE Groups SET name_of_group='${req.body.saveNameGroup}' WHERE id='${req.params.id}'`)
   res.redirect('/groups')
+})
+
+app.get('/profiles', function(req, res){
+  db.all('SELECT Profiles.id, Profiles.username, Profiles.password, Profiles.Contact_id, Contacts.name FROM Profiles JOIN Contacts ON Profiles.Contact_id = Contacts.id;', function(err, rows){
+    if(!err){
+      db.all('SELECT * FROM Contacts;', function(err, data){
+        if(!err){
+          res.render('profiles', {data_profile : rows, data_contact : data})
+        }
+      })
+    }
+  })
+})
+
+app.post('/profiles', function(req, res){
+  db.run(`INSERT INTO Profiles(username, password, Contact_id) VALUES ('${req.body.saveUsername}', '${req.body.savePassword}', '${req.body.saveContactID}');`)
+  res.redirect('/profiles');
+})
+
+app.get('/profiles/delete/:id', function(req, res){
+  db.run(`DELETE FROM Profiles WHERE id=${req.params.id};`);
+  res.redirect('/profiles')
+})
+
+app.get('/profiles/edit/:id', function(req, res, next){
+  db.each(`SELECT * FROM Profiles WHERE id=${req.params.id};`, function(err, data){
+    if(!err){
+      db.all(`SELECT * FROM Contacts`, function(err, rows){
+        if(!err){
+        res.render('editProfiles', {dataProfile : data, dataContact : rows})
+      }
+      })
+    }
+  })
+})
+
+app.post('/profiles/edit/:id', function(req, res){
+  db.run(`UPDATE Profiles SET username='${req.body.saveUsername}',
+  password='${req.body.savePassword}',
+  Contact_id='${req.body.saveContactID}' WHERE id=${req.params.id};`)
+  res.redirect('/profiles')
 })
 
 
